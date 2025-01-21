07cdc792ba0bc271aca7bb11f136d74f884c5558
Jeremy Garcia <Jeremygarcia9400@gmail.com>
2025-01-21 18:53:52 -0500

    reforamtted js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import express from 'express';
+import express from "express";
 const app = express();
 
-app.get('/', (req, res) => 
-    res.json({message: 'Docker if easy 🐳'})
-);
+app.get("/", (req, res) => res.json({ message: "Docker if easy 🐳" }));
 
 const port = process.env.PORT || 8080;
 
-app.listen(port, () => console.log(`app listening on http://localhost:${port}`));
+app.listen(port, () =>
+  console.log(`app listening on http://localhost:${port}`)
+);
